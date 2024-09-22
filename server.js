7a565eac72ab04f639612b89cbff9ac46bbe45e3
Robyn <Robyn796@mac.com>
2024-09-21 21:34:30 -0600

    mongodb

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const mongodb = requre('./data/database');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 
 app.use('/', require('./routes'));
 
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+
+    }
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Database is listening and node running on port ${PORT}`);
 });
