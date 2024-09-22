f1be9605b00b55aa76fe57b08b8853c8e0cce808
Robyn <Robyn796@mac.com>
2024-09-21 18:44:56 -0600

    update

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-// server.js
-
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
-// Define a route to return "Hello World"
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
